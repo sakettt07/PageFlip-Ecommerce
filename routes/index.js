1d3ea5a557a388184c4d532c6556070b0f4b1147
saket <saket@saketgupta.com>
2024-07-09 01:41:56 +0530

    completed the edit functionality for the user he can change the profile picture and the username

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const isLoggedIn=require("../middlewares/isLoggedIn.js");
 const productModels = require("../models/product.models.js");
 const userModels = require("../models/user.models.js");
+const upload =require("../config/multer-config.js");
 
 function formatDate(date) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -55,5 +56,39 @@ router.get("/account",isLoggedIn,async function(req,res){
         res.status(500).send("Server Error");
     }
 
+})
+router.get("/edit/:id",isLoggedIn,async function(req,res){
+    try {
+        let user=await userModels.findOne({_id:req.params.id});
+        if(!user){
+            return res.status(404).send("User not found")
+        }
+        res.render('edit',{user});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
+router.post("/edit/:id",upload.single("profilePicture"),isLoggedIn,async function(req,res){
+    try {
+        let user=await userModels.findById(req.params.id);
+        if(!user){
+            return res.status(404).send("User not found");
+        }
+        if (req.body.fullname) {
+            user.fullname = req.body.fullname;
+        }
+
+        // Update profile picture if uploaded
+        if (req.file) {
+            // Convert buffer to base64 and save
+            user.picture = req.file.buffer;
+        }
+        await user.save();
+        res.redirect('/account')
+    } catch (error) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');  
+    }
 })
 module.exports=router;
